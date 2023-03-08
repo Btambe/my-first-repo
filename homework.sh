dea8ae70d794381be986f389dd2d4dfced816a54
Beverly Tambe <btambe2@gmail.com>
2023-03-08 14:46:49 +0000

    starting homework 1

---
+++ homework.sh
@@ -0,0 +1,3 @@
+#Create a bash script that takes 2 numbers (that should be taken as CLI arguments: $1 and $2), and prints the larger number on the screen.  (‘read’ command is not necessary since you’re supplying it as a CLI argument.)
+
+#!/bin/bash
