406ae422546ea05af67f0ddf62be81ad3ae52b48
Beverly Tambe <btambe2@gmail.com>
2023-03-08 16:40:48 +0000

    bash script 1

--- homework.sh
+++ homework.sh
@@ -1,3 +1,13 @@
 #Create a bash script that takes 2 numbers (that should be taken as CLI arguments: $1 and $2), and prints the larger number on the screen.  (‘read’ command is not necessary since you’re supplying it as a CLI argument.)
 
 #!/bin/bash
+
+echo $1
+echo $2
+
+if [ $1 -gt $2 ]
+then
+	        echo "$1 is greater."
+	else
+	        echo "$2 is greater."
+fi
